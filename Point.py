0b973a44c0fb053af64671135f0495fd969045e2
lccastro9 <luisk9-_@hotmail.com>
2017-06-28 11:11:25 -0500

    Add files via upload

---
+++ Point.py
@@ -0,0 +1,14 @@
+class Point():
+    def __init__(self,x,y):
+        self.x=x 
+        self.y=y 
+    def move(self,x,y):
+        self.x1=x
+        self.y1=y   
+    def reset(self):
+        self.x=0
+        self.y=0   
+    def calculate_distance(self,otherPoint):
+        a=(otherPoint.x-self.x)**2 + (otherPoint.y-self.y)**2
+        distancia = a**0.5
+        return distancia
